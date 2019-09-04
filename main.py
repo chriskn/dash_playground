9588753d66a31a3abcdfafa8f0b80837cf17b710
chriskn <christoph.knauf@t-online.de>
2019-09-04 19:48:53 +0200

    single page

--- main.py
+++ main.py
@@ -3,12 +3,67 @@ import dash
 import dash_table
 from datetime import datetime as dt
 import dash_core_components as dcc
-import package_complexity
 import dash_html_components as html
 import pandas as pd
+import flask
+import figure
+import dash_bootstrap_components as dbc
 
 app = dash.Dash("Sat")
 app.title = "Sat"
+
+dataframe = pd.read_csv(
+    "package_complexity.csv", sep="\s*;\s*", header=0, encoding="ascii", engine="python"
+)
+dataframe = dataframe.drop(columns=["Path"])
+
+main_content = html.Div(
+    className="app_main_content",
+    children=[
+        html.Div(id="top-row", className="row"),
+        html.Div(id="middle-row", className="row"),
+        html.Div(
+            id="bottom-row",
+            className="row",
+            children=[
+                html.Div(
+                    id="table",
+                    className="twelve columns",
+                    children=dcc.Loading(
+                        id="tabe-loading",
+                        children=dash_table.DataTable(
+                            id="data-table",
+                            columns=[{"name": i, "id": i} for i in dataframe.columns],
+                            filter_action="native",
+                            sort_action="native",
+                            sort_mode="multi",
+                            fill_width=True,
+                            data=dataframe.to_dict("records"),
+                            style_header={
+                                "textTransform": "Uppercase",
+                                "fontWeight": "bold",
+                                "backgroundColor": "#ffffff",
+                                "padding": "10px 0px",
+                            },
+                            style_cell={"textAlign": "left"},
+                            style_data_conditional=[
+                                {
+                                    "if": {"row_index": "even"},
+                                    "backgroundColor": "#f5f6f7",
+                                },
+                                {
+                                    "if": {"row_index": "odd"},
+                                    "backgroundColor": "#ffffff",
+                                },
+                            ],
+                        ),
+                    ),
+                )
+            ],
+        ),
+    ],
+)
+
 
 app.layout = html.Div(
     className="container scalable",
@@ -26,36 +81,65 @@ app.layout = html.Div(
             children=html.Div(
                 [
                     html.H5("Reports from 30.08.2019 14:21:14"),
-                    dcc.Tabs(
-                        id="tabs",
-                        value="tab-1",
-                        children=[
-                            dcc.Tab(label="Package complexity", value="tab-1"),
-                            dcc.Tab(label="Tab Two", value="tab-2"),
-                        ],
-                    ),
-                    html.Div(id="tabs-content"),
+                    main_content
                 ]
             ),
         ),
     ],
 )
 
+def _chart(cssClassName="", chart=None):
+    return html.Div(className=cssClassName, children=dcc.Loading(children=chart))
 
-@app.callback(Output("tabs-content", "children"), [Input("tabs", "value")])
-def render_content(tab):
-    if tab == "tab-1":
-        return package_complexity.content
-    elif tab == "tab-2":
-        return html.Div(
-            [
-                html.H3("Tab content 2"),
-                dcc.Graph(
-                    id="graph-2-tabs",
-                    figure={"data": [{"x": [1, 2, 3], "y": [5, 10, 6], "type": "bar"}]},
-                ),
-            ]
-        )
+
+@app.callback(
+    Output("top-row", "children"), [Input("data-table", "derived_virtual_data")]
+)
+def update_top(data):
+    data = pd.DataFrame.from_dict(data) if data else dataframe
+    return _chart(
+        cssClassName="twelve columns",
+        chart=figure.scatter(
+            "Package Complexity & Size",
+            data,
+            "Package",
+            "Complexity",
+            "Number of statements",
+            "Average class complexity",
+            "Average method complexity",
+        ),
+    )
+
+
+@app.callback(
+    Output("middle-row", "children"), [Input("data-table", "derived_virtual_data")]
+)
+def update_middle(data):
+    data = pd.DataFrame.from_dict(data) if data else dataframe
+    return [
+        _chart(
+            cssClassName="six columns",
+            chart=figure.barchart(
+                "Number of classes interfaces and enums in package",
+                data,
+                "Number of types",
+                "Package",
+                50,
+            ),
+        ),
+        _chart(
+            cssClassName="six columns",
+            chart=dcc.Loading(
+                children=figure.barchart(
+                    "Number of methods in package",
+                    data,
+                    "Number of methods",
+                    "Package",
+                    50,
+                )
+            ),
+        ),
+    ]
 
 
 if __name__ == "__main__":
